42f766fe2d9dbd4752adb0c66c48eda078e86ba2
Merlin Nimier-David <merlin.nimier@gmail.com>
2016-05-14 21:11:03 +0200

    Break when a proper coloring is found (+ small adjustments)

--- code/project.m
+++ code/project.m
@@ -7,32 +7,31 @@ addpath(genpath('lib'));
 %% Setup
 
 % Generate test adjacency matrix
-N = 1000;             % Number of edges
-q = 50;              % Number of colors
+N = 100;             % Number of edges
+q = 5;              % Number of colors
 beta = 0.2;         % Inverse temperature
-c = 30;              % Density
+c = 3;              % Density
 
-% Erd\"os-R\'enyi graph
+% Erdos-Renyi graph
 G = rand(N) < c/N;  % Assign edges
 G = triu(G,1);      % Ensure symmetric
 G = G | G';
 
 %% Initial coloring
-x = randsample(q,N,true);
+x = randsample(q, N, true);
 
-visualizeGraph(G,x,'Original graph');
+visualizeGraph(G, x, 'Original graph');
 export_fig('/tmp/original.pdf');  % High-quality figure for report
 %return;
 
 %% Metropolis
 
 n = 5000;  % Number of iterations
+x = randsample(q, N, true);
 current_energy = H(G, x);
 
 % Initialize optimization visualization
 energies = zeros(n, 1);
-figure; h = animatedline; xlim([1, n]); ylim([0, current_energy]);
-title('Energy'); xlabel('Iterations'); ylabel('Cost');
 
 % Metropolis iteration
 tic;
@@ -40,32 +39,31 @@ for i=1:n
     v = randsample(N,1);              % Selected vertex
     v_c = randsample(q-1,1);          % New color
     if v_c >= x(v), v_c = v_c+1; end  % Assign color different from current
-    x_new = x; x_new(v) = v_c;
+    x_new = x;
+    x_new(v) = v_c;
     
     new_energy = H(G, x_new);
     delta_E = new_energy - current_energy;      % Energy difference
     beta = getNextBeta(i,n);
     % Accept if lower energy or with acceptance probability:
-    accept = rand(1)<=min(1,exp(-beta*delta_E));
+    accept = rand(1) <= min(1, exp(-beta * delta_E));
 
     if accept
         x = x_new;
         current_energy = new_energy;
     end
     
-    % Update plot
-    %addpoints(h, i, current_energy); drawnow;
     energies(i) = current_energy;
     
-%     if current_energy==0
-%         fprintf('Proper coloring found! ');
-%         disp(x');
-%         visualizeGraph(G,x,sprintf('Proper coloring %i',i));
-    %else
-    %    visualizeGraph(G,x,sprintf('Non proper coloring %i',i));
-    %end
+    if current_energy == 0
+        fprintf('Proper coloring found! ');
+        break;
+    end
 end
 toc;
-plot(energies);
 
+% TODO: export properly colored graph and energy plot
+figure;
+plot(energies(1:i));
+title('Energy'); xlabel('Iterations'); ylabel('Cost');
     
