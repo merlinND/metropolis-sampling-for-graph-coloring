83819fa16b61e093d32d0edf23cb0550df898390
Victor Kristof <victor.kristof@epfl.ch>
2016-05-23 12:14:14 +0200

    Adapted schedule comparison with new parameter values

--- code/evaluateSchedules.m
+++ code/evaluateSchedules.m
@@ -5,50 +5,45 @@
 % you lose the advantage of the Metropolis step and might end up in a local
 % minimum with strictly positive cost.
 
-clear; close all;
+% clear; close all;
 
 % TODO: tweak these values
 max_iterations = 5000;
-N = 100;
-q = 3;
-cValues = linspace(0, N, 20);
+N = 250;
+q = 10;
+cValues = linspace(0, N/4, 10);
 % Since we're working with random graphs, we make several random draws
 % to make sure that we obtain relevant data points.
 nDraws = 5;
 
 % Define the schedules to evaluate
-% Constant
-beta_0 = 100;
-constantArgs = {beta_0};
 % Linear
-beta_0 = 100;
-alpha = 0.01;
-linearArgs = {beta_0, alpha};
+beta_0 = 0.001;
+alpha = 6;
+tau = 0.3;
+linearArgs = {beta_0, alpha, tau};
 % Exponential
-beta_0 = 0;
-c = 1000;
-alpha = 1;
-delta = 1;
-tau = 50;
-exponentialArgs = {beta_0, c, alpha, delta, tau};
+beta_0 = 0.1;
+alpha = 0.005;
+tau = 0.1;
+exponentialArgs = {beta_0, alpha, tau};
 % Logarithmic
 beta_0 = 10;
-tau = 1;
-logarithmicArgs = {beta_0, tau};
+alpha = 1;
+tau = 0;
+logarithmicArgs = {beta_0, alpha, tau};
 % Polynomial 
-beta_0 = 10;
+beta_0 = 3;
 alpha = 2;
-polynomialArgs = {beta_0, alpha};
+tau = 0;
+polynomialArgs = {beta_0, alpha, tau};
 % Adaptive exponential
 beta_0 = 0;
-c = 1000;
 alpha = 1;
-delta = 1;
-tau = 50;
-adaptiveArgs = {beta_0, c, alpha, delta, tau};
+tau = 0;
+adaptiveArgs = {beta_0, c, alpha, tau};
 
-schedules = {'constant', constantArgs;
-             'linear', linearArgs;
+schedules = {'linear', linearArgs;
              'exponential', exponentialArgs;
              'logarithmic', logarithmicArgs;
              'polynomial', polynomialArgs;
@@ -69,7 +64,7 @@ for k = 1:length(schedules)
             G = randomGraph(N, c);
             [~, minimum_energies(i, j, k)] = findColoring(G, q, max_iterations, schedule, scheduleArgs);
         end;
-    fprintf('#');
+        fprintf('#');
     end;
     fprintf('\n');
 end;
@@ -83,8 +78,8 @@ for k = 1:length(schedules)
     errorbar(cValues, m, u, '-+');
     legends{end+1} = sprintf('%s', schedules{k, 1});
 end;
-title_string = sprintf('Hmin(q, c) with N=%d edges (max iterations: %d)', ...
-                       N, max_iterations);
-title(title_string); xlim([0 N]);
+title_string = sprintf('Hmin(q=%d, c) with N=%d edges (max iterations: %d)', ...
+                       q, N, max_iterations);
+title(title_string); xlim([0 cValues(end)]);
 xlabel('Graph density (c)'); ylabel('Minimum energy achieved');
 legend(legends, 'Location', 'NorthWest');
