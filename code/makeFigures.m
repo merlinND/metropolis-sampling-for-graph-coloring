967ce3124769bca273754042d5d0d9637fa198af
Merlin Nimier-David <merlin.nimier@gmail.com>
2016-05-23 12:08:11 +0200

    makeFigures: a couple more useful figures

--- code/makeFigures.m
+++ code/makeFigures.m
@@ -17,3 +17,43 @@ visualizeGraph(G, x, str);
 filename = sprintf('../figures/random-graph-%d-%d-%d.pdf', N, c, q);
 export_fig(filename);  % High-quality figure for report
 %return;
+
+%% Initial (random) normalized cost w.r.t. graph density
+nValues = [100, 500, 1000]; %linspace(1, 1000, 100);
+q = 5;
+cValues = 20;
+costs = zeros(length(nValues), cValues);
+for i = 1:length(nValues)
+    N = nValues(i);
+    cc = linspace(1, N, cValues);
+    for j = 1:length(cc)
+        c = cc(j);
+        G = randomGraph(N, c);
+        x = randsample(q, N, true);
+        costs(i, j) = H(G, x) / N;
+    end;
+end;
+
+figure;
+plot(linspace(0, 1, cValues), costs, '+');
+
+% Relationship:
+% initialCost / N = c / (2 * q)
+
+%% Max degree VS density
+
+nValues = [500]; %linspace(1, 1000, 100);
+cValues = 200;
+maxDegrees = zeros(length(nValues), cValues);
+for i = 1:length(nValues)
+    N = nValues(i);
+    cc = linspace(1, N, cValues);
+    for j = 1:length(cc)
+        c = cc(j);
+        G = randomGraph(N, c);
+        maxDegrees(i, j) = max(sum(G, 1));
+    end;
+end;
+
+figure;
+plot(linspace(0, 1, cValues), maxDegrees, '+');
