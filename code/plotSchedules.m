aa67ecf333b320889736fc026ad7b1828d0ef567
Roger Bermudez-Chacon <roger.bermudez@epfl.ch>
2016-05-25 11:18:45 +0200

    Discretized schedules

--- code/plotSchedules.m
+++ code/plotSchedules.m
@@ -1,6 +1,7 @@
 function [ ] = plotSchedules( schedules )
 %PLOTSCHEDULESF Summary of this function goes here
 %   Detailed explanation goes here
+discSch = @(x) log(7*x+1) / log(8);
 
 figure; hold on;
 x = linspace(0, 1, 1000);
@@ -9,7 +10,13 @@ for k = 1:length(schedules)
     schedule_id = schedules{k, 1};
     schedule = getSchedule(schedule_id);
     scheduleArgs = schedules{k, 2};
-    plot(x, schedule(x, scheduleArgs{:}));
+
+    newx = discSch(x);
+    binBounds = linspace(0,1,10);
+    newx = discretize(newx, binBounds, binBounds(2:end));
+    betas = schedule(newx, scheduleArgs{:});
+
+    plot(x, betas);
     legends{end+1} = sprintf('%s', schedules{k, 1});
 end;
 title('Schedules for \beta(t)', 'FontSize', 16); xlim([0 1]);
